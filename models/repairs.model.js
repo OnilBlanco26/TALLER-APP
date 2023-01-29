ea184ccd6b3f1f8534375ec9a103b8dd7e4be2b9
OnilBlanco26 <onilblanco26@gmail.com>
2023-01-29 19:33:44 -0300

    repairs model

---
+++ models/repairs.model.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require('sequelize');
+const { db } = require('../database/db');
+
+const Repairs = db.define('repair', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  date: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  status: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: 'pending',
+    enum: ['pending', 'completed', 'cancelled'],
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
+
+module.exports = Repairs;
