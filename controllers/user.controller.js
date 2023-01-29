41e9c087f0bead1518d95fc38fab8adc7285b1ae
OnilBlanco26 <onilblanco26@gmail.com>
2023-01-29 19:33:09 -0300

    adding  try catch to user.controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,114 +1,153 @@
 const User = require('../models/user.model');
 
 const findUsers = async (req, res) => {
-  const users = await User.findAll({
-    where: {
-      status: true,
-    },
-  });
+  try {
+    const users = await User.findAll({
+      where: {
+        status: 'available',
+      },
+    });
 
-  res.json({
-    status: 'success',
-    message: 'the user where found succesfully',
-    users,
-  });
+    res.json({
+      status: 'success',
+      message: 'the users were found successfully',
+      users,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 'error',
+      message: 'failed to find users',
+      error,
+    });
+  }
 };
 
 const findUser = async (req, res) => {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-  const user = await User.findOne({
-    where: {
-      id,
-      status: true,
-    },
-  });
+    const user = await User.findOne({
+      where: {
+        id,
+        status: 'available',
+      },
+    });
 
-  if (!user) {
-    res.status(404).json({
+    if (!user) {
+      res.status(404).json({
+        status: 'error',
+        message: 'the user was not found',
+      });
+      return;
+    }
+
+    res.json({
+      status: 'success',
+      message: 'user was found successfully',
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({
       status: 'error',
-      message: 'the user was not found',
+      message: 'failed to find user',
+      error,
     });
   }
-
-  res.json({
-    status: 'success',
-    message: 'user has found succesfully',
-    user,
-  });
 };
 
 const createUser = async (req, res) => {
-  const { name, email, password, role } = req.body;
-  const newUser = await User.create({
-    name: name.toLowerCase(),
-    email: email.toLowerCase(),
-    password,
-    role,
-  });
+  try {
+    const { name, email, password, role } = req.body;
+    const newUser = await User.create({
+      name: name.toLowerCase(),
+      email: email.toLowerCase(),
+      password,
+      role,
+    });
 
-  res.status(201).json({
-    status: 'success',
-    message: 'the user was created succesfully',
-    newUser,
-  });
+    res.status(201).json({
+      status: 'success',
+      message: 'user was created successfully',
+      newUser,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 'error',
+      message: 'failed to create user',
+      error,
+    });
+  }
 };
 
 const updateUser = async (req, res) => {
-  const { id } = req.params;
-  const { name, email, password, role } = req.body;
+  try {
+    const { id } = req.params;
+    const { name, email } = req.body;
 
-  const user = await User.findOne({
-    where: {
-      id,
-      status: true,
-    },
-  });
+    const user = await User.findOne({
+      where: {
+        id,
+        status: 'available',
+      },
+    });
 
-  const updateUser = await user.update({
-    name: name.toLowerCase(),
-    email: email.toLowerCase(),
-    password,
-    role,
-  });
+    if (!user) {
+      res.status(404).json({
+        status: 'error',
+        message: 'user was not found',
+      });
+      return;
+    }
 
-  if (!user) {
-    res.status(404).json({
+    const updateUser = await user.update({
+      name: name.toLowerCase(),
+      email: email.toLowerCase(),
+    });
+
+    res.json({
+      status: 'success',
+      message: 'user was updated successfully',
+      updateUser,
+    });
+  } catch (error) {
+    res.status(500).json({
       status: 'error',
-      message: 'user was not found',
+      message: 'failed to update user',
+      error,
     });
   }
-
-  res.json({
-    status: 'success',
-    message: 'user was updated succesfully',
-    updateUser,
-  });
 };
 
 const deleteUser = async (req, res) => {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-  const user = await User.findOne({
-    where: {
-      id,
-      status: true,
-    },
-  });
+    const user = await User.findOne({
+      where: {
+        id,
+        status: 'available',
+      },
+    });
 
-  if (!user) {
-    res.status(404).json({
+    if (!user) {
+      return res.status(404).json({
+        status: 'error',
+        message: 'user was not found',
+      });
+    }
+
+    await user.update({ status: 'not available' });
+
+    return res.json({
+      status: 'success',
+      message: 'user delete was succesfully',
+    });
+  } catch (err) {
+    return res.status(500).json({
       status: 'error',
-      message: 'user was not found',
+      message: 'something went wrong',
     });
   }
-
-  await user.update({ status: false });
-
-  res.json({
-    status: 'success',
-    message: 'user delete was succesfully',
-  });
 };
 
 module.exports = {
